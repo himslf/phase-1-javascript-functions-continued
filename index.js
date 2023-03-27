3d9a3a02a96de39cdff43f8b545b18cfdff45077
JASON YOUNG <moorslf@gmail.com>
2023-03-27 14:40:02 -0400

    Functions Cont'd

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = (function (mondayAct = "go to the office") {
+    return `This Monday, I will ${mondayAct}.`;
+});
+
+// const wrapAdjective = function (flair = '*') {
+//     return function (adjective = 'special') {
+//         return 'You are ${flair} ${adjective} ${flair}!';
+//     }
+// }; gotta practice some more
+
+function wrapAdjective(flair = "*") {
+     return function (adjective = "special"){
+        return `You are ${flair}${adjective}${flair}!`
+    }
+};
